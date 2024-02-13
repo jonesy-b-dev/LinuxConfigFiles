9c4014a7f87cc3f25ad29f151ee06197e25b8f71
Jonas de Bruin <jonasdebruin05@gmail.com>
2024-02-13 12:26:18 +0100

    Added options for installing specific configs

--- install.sh
+++ install.sh
@@ -1,10 +1,55 @@
 #!/bin/bash
+
 # Installs the config files in the repository to the system
 # WARNING this overwrites the current files in the repository.
 
+install_bash=false
+install_nvim=false
 
-# Copy bash files
-cp bash/.bashrc bash/.bash_aliases ~/
+# Parse cl option
+while getopts "bv" opt; do
+	case ${opt} in
+		b) 
+        install_bash=true ;;
+		v) 
+        install_nvim=true ;;
+	esac
+done
 
-# Copy nvim fies
-cp -r nvim/ ~/.config/
+# If no options provided, perform full install
+if [[ $# -eq 0 ]]; then
+    install_bash=true
+    install_nvim=true
+fi
+
+# Function to install bash config
+install_bash_config() {
+    echo "Installing bash config..."
+    cp bash/.bashrc bash/.bash_aliases ~/
+    # Add commands to install bash config files here
+}
+
+# Function to install vim config
+install_nvim_config() {
+    echo "Installing nvim config..."
+    cp -r nvim/ ~/.config/
+    echo "Bah configs installed, please run [source ~./bashrc] to complete the setup."
+    # Add commands to install vim config files here
+}
+
+# Main installation function
+install_configs() {
+    if $install_bash; then
+        install_bash_config
+    fi
+
+    if $install_nvim; then
+        install_nvim_config
+    fi
+
+    # Add more config installation functions here as needed
+}
+
+
+# Execute installation
+install_configs
