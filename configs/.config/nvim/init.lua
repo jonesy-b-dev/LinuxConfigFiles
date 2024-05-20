e02c1664c293a572900d0947068d8a590eeb0ab1
Jonas de Bruin <59418915+jonesy-b-dev@users.noreply.github.com>
2024-05-20 17:10:19 +0200

    Terminal colors for neovim

--- configs/.config/nvim/init.lua
+++ configs/.config/nvim/init.lua
@@ -1,9 +1,13 @@
+-- Set (Relative) lines
 vim.wo.number = true
 vim.wo.relativenumber = true
 
+-- Set tab spacing
 vim.opt.shiftwidth=4
 vim.opt.tabstop=4
 
+-- Set terminal colors
+vim.opt.tgc = false
 
 --- REMAPS
 vim.api.nvim_set_keymap('n', '<C-d>', '<C-d>zz', { noremap = true, silent = true})
