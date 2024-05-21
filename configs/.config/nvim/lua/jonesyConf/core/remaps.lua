136289f14b64b90a51594f9f8b123d949fdfe109
Jonas de Bruin <59418915+jonesy-b-dev@users.noreply.github.com>
2024-05-21 20:25:45 +0200

    Added improved tab switching

--- configs/.config/nvim/lua/jonesyConf/core/remaps.lua
+++ configs/.config/nvim/lua/jonesyConf/core/remaps.lua
@@ -6,3 +6,8 @@ vim.api.nvim_set_keymap('n', '<C-e>', '<C-e>zz', { noremap = true, silent = true
 vim.api.nvim_set_keymap('n', 'n', 'nzzzv', { noremap = true, silent = true})
 vim.api.nvim_set_keymap('n', 'N', 'Nzzzv', { noremap = true, silent = true})
 
+-- Remap window navigation to Ctrl + h/j/k/l
+vim.api.nvim_set_keymap('n', '<C-h>', '<C-w>h', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<C-j>', '<C-w>j', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<C-k>', '<C-w>k', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<C-l>', '<C-w>l', { noremap = true, silent = true })
