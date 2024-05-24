5652164b0c4457dab5285c71e0a8b53a29c9f85f
Jonas de Bruin <jonasdebruin05@gmail.com>
2024-05-24 12:54:43 +0200

    Added undotree plugin and keybind

--- configs/.config/nvim/lua/jonesyConf/core/remaps.lua
+++ configs/.config/nvim/lua/jonesyConf/core/remaps.lua
@@ -11,3 +11,6 @@ vim.api.nvim_set_keymap('n', '<C-h>', '<C-w>h', { noremap = true, silent = true 
 vim.api.nvim_set_keymap('n', '<C-j>', '<C-w>j', { noremap = true, silent = true })
 vim.api.nvim_set_keymap('n', '<C-k>', '<C-w>k', { noremap = true, silent = true })
 vim.api.nvim_set_keymap('n', '<C-l>', '<C-w>l', { noremap = true, silent = true })
+
+-- Custom plugin shortcuts
+vim.keymap.set('n', '<F2>', vim.cmd.UndotreeToggle)
