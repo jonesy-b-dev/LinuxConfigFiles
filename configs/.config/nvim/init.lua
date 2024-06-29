4beabedfd5bc14621053026aefa09a33d027f920
Jonas de Bruin <59418915+jonesy-b-dev@users.noreply.github.com>
2024-06-29 16:34:14 +0200

    Added some search options to nvim

--- configs/.config/nvim/init.lua
+++ configs/.config/nvim/init.lua
@@ -13,6 +13,11 @@ vim.opt.tabstop = 4
 -- Set terminal colors
 vim.opt.tgc = false
 
+-- Set search options
+vim.opt.ignorecase = true 
+vim.opt.smartcase = true -- search matters if capital letter
+vim.opt.inccommand = "split" -- "for incsearch while sub
+
 -- Turn on syntax highlighting
 vim.api.nvim_create_autocmd("VimEnter", {
   pattern = "*",
