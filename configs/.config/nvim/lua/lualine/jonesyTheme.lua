321175c67cfcbd5c6235cccd32965bc72c682910
Jonas de Bruin <59418915+jonesy-b-dev@users.noreply.github.com>
2024-10-31 21:17:59 +0100

    Fixed some lualine being invisible

--- configs/.config/nvim/lua/lualine/jonesyTheme.lua
+++ configs/.config/nvim/lua/lualine/jonesyTheme.lua
@@ -5,7 +5,7 @@ local colors = {
 	  green        = '#b8bb26',
 	  blue         = '#458588',
 	  yellow       = '#ffab1f',
-	  magenta	   = '#b8329b',
+	  magenta      = '#b8329b',
 	  gray         = '#2e2b28',
 	  darkgray     = '#2e2b28',
 	  lightgray    = '#3b3b38',
@@ -15,7 +15,7 @@ return {
   normal = {
     a = {bg = colors.yellow, fg = colors.black, gui = 'bold'},
     b = {bg = colors.lightgray, fg = colors.white},
-    c = {bg = colors.darkgray, fg = colors.gray}
+    c = {bg = colors.darkgray, fg = colors.white}
   },
   insert = {
     a = {bg = colors.green, fg = colors.black, gui = 'bold'},
@@ -25,7 +25,7 @@ return {
   visual = {
     a = {bg = colors.blue, fg = colors.black, gui = 'bold'},
     b = {bg = colors.lightgray, fg = colors.white},
-    c = {bg = colors.darkgray, fg = colors.black}
+    c = {bg = colors.darkgray, fg = colors.white}
   },
   replace = {
     a = {bg = colors.red, fg = colors.black, gui = 'bold'},
@@ -35,7 +35,7 @@ return {
   command = {
     a = {bg = colors.magenta, fg = colors.black, gui = 'bold'},
     b = {bg = colors.lightgray, fg = colors.white},
-    c = {bg = colors.darkgray, fg = colors.black}
+    c = {bg = colors.darkgray, fg = colors.white}
   },
   inactive = {
     a = {bg = colors.darkgray, fg = colors.gray, gui = 'bold'},
