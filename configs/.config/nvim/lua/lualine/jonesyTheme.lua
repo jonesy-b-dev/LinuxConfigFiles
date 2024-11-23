e6c33bde0771e00706f21e0d8badd5b90eab43ce
Jonas de Bruin <jonasdebruin@proton.me>
2024-11-23 12:03:56 +0100

    Improved lualine theme

--- configs/.config/nvim/lua/lualine/jonesyTheme.lua
+++ configs/.config/nvim/lua/lualine/jonesyTheme.lua
@@ -1,41 +1,41 @@
 local colors = {
 	  black        = '#333232',
-	  white        = '#a89984',
+	  white        = '#e6d8c8',
 	  red          = '#cc241d',
 	  green        = '#b8bb26',
 	  blue         = '#458588',
 	  yellow       = '#ffab1f',
-	  magenta      = '#b8329b',
+	  magenta	   = '#b8329b',
 	  gray         = '#2e2b28',
 	  darkgray     = '#2e2b28',
 	  lightgray    = '#3b3b38',
-	  inactivegray = '#7c6f64',
+	  inactivegray = '#57504b',
 }
 return {
   normal = {
     a = {bg = colors.yellow, fg = colors.black, gui = 'bold'},
-    b = {bg = colors.lightgray, fg = colors.white},
-    c = {bg = colors.darkgray, fg = colors.white}
+    b = {bg = colors.inactivegray, fg = colors.white},
+    c = {bg = colors.lightgray, fg = colors.white}
   },
   insert = {
     a = {bg = colors.green, fg = colors.black, gui = 'bold'},
-    b = {bg = colors.lightgray, fg = colors.white},
-    c = {bg = colors.darkgray, fg = colors.white}
+    b = {bg = colors.inactivegray, fg = colors.white},
+    c = {bg = colors.lightgray, fg = colors.white}
   },
   visual = {
     a = {bg = colors.blue, fg = colors.black, gui = 'bold'},
-    b = {bg = colors.lightgray, fg = colors.white},
-    c = {bg = colors.darkgray, fg = colors.white}
+    b = {bg = colors.inactivegray, fg = colors.white},
+    c = {bg = colors.lightgray, fg = colors.white}
   },
   replace = {
     a = {bg = colors.red, fg = colors.black, gui = 'bold'},
-    b = {bg = colors.lightgray, fg = colors.white},
+    b = {bg = colors.inactivegray, fg = colors.white},
     c = {bg = colors.black, fg = colors.white}
   },
   command = {
     a = {bg = colors.magenta, fg = colors.black, gui = 'bold'},
-    b = {bg = colors.lightgray, fg = colors.white},
-    c = {bg = colors.darkgray, fg = colors.white}
+    b = {bg = colors.inactivegray, fg = colors.white},
+    c = {bg = colors.lightgray, fg = colors.white}
   },
   inactive = {
     a = {bg = colors.darkgray, fg = colors.gray, gui = 'bold'},
