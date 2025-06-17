b7c40e3a54af8285dc351671bfd0db6589d25cdf
Jonas de Bruin <jonasdebruin@proton.me>
2025-06-17 17:49:32 +0000

    Remove shell scripts, not really needed

--- update.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-mapfile -t configsLc < <(ls configs/.config)
-mapfile -t configsEx < <(ls ~/.config)
-
-echo "Local configs"
-for conf in "${configsLc[@]}"; do
-	cp -r ~/.config/$conf ~/temp
-done
