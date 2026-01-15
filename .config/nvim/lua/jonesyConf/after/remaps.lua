be5e97cddb9bae37a516889de9c454586ce337a9
Jonas de Bruin <59418915+jonesy-b-dev@users.noreply.github.com>
2026-01-15 14:18:31 +0100

    [Nvim] Added shortcut to search with hidden files

--- .config/nvim/lua/jonesyConf/after/remaps.lua
+++ .config/nvim/lua/jonesyConf/after/remaps.lua
@@ -3,3 +3,9 @@ vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
 vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
 vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
 vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
+vim.keymap.set('n', '<leader>fj', function()
+    require('telescope.builtin').find_files({
+        hidden = true,
+        no_ignore = true,
+    })
+end)
