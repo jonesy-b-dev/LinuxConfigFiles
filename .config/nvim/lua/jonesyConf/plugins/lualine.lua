b9442fe22ad440d609311978e1044528cb888588
Jonas de Bruin <jonasdebruin@proton.me>
2026-02-03 17:22:50 +0100

    [Nvim] Show lsp in luabar

--- .config/nvim/lua/jonesyConf/plugins/lualine.lua
+++ .config/nvim/lua/jonesyConf/plugins/lualine.lua
@@ -25,7 +25,7 @@ return {
 		    lualine_a = {'mode'},
 		    lualine_b = {'branch', 'diff', 'diagnostics'},
 		    lualine_c = {'filename'},
-		    lualine_x = {'encoding', 'fileformat', 'filetype'},
+		    lualine_x = {'encoding', 'fileformat', 'filetype', 'lsp_status'},
 		    lualine_y = {'progress'},
 		    lualine_z = {'location'}
 		  },
