ae3033fe52c5e983de3cd27cd382f1fb91686859
Jonas de Bruin <59418915+jonesy-b-dev@users.noreply.github.com>
2026-02-06 15:28:29 +0100

    [Nvim] Fixed TreeSitter AGAIN

--- .config/nvim/lua/jonesyConf/plugins/treesitter.lua
+++ .config/nvim/lua/jonesyConf/plugins/treesitter.lua
@@ -3,31 +3,13 @@ return {
 	lazy = false,
 	build = ":TSUpdate",
 	config = function()
-		local configs = require("nvim-treesitter")
-
-		configs.setup({
-			ensure_installed = {
-				"cpp",
-				"c_sharp",
-				"vim",
-				"vimdoc",
-				"javascript",
-				"html",
-				"css",
-				"lua",
-			},
-			sync_install = false,
-			auto_install = true,
-			highlight = {
-				enable = true,
-				additional_vim_regex_highlighting = false,
-			},
-		})
-
-		-- Enable highlighting via autocmd
+		-- Ensure parsers are installed (enable 1 time after new nvim install to quickly install needed parsers)
+		--vim.cmd("TSInstall cpp c_sharp vim vimdoc javascript html css lua")
+		
+		-- Enable highlighting
 		vim.api.nvim_create_autocmd('FileType', {
 			callback = function()
-				vim.treesitter.start()
+				pcall(vim.treesitter.start)
 			end,
 		})
 	end
