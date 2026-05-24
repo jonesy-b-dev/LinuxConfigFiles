ff28b715399f051b2a86523eaff30ca08142b6b3
Jonas de Bruin <59418915+jonesy-b-dev@users.noreply.github.com>
2026-05-24 15:26:04 +0200

    [Hypr] Fix workspace 6-9 to be inaccessible due to missing keybinds

--- .config/hypr/hyprland-lua/hyprWorkSpaceMultiMonitor.lua
+++ .config/hypr/hyprland-lua/hyprWorkSpaceMultiMonitor.lua
@@ -3,7 +3,7 @@
 local smw = hl.plugin.split_monitor_workspaces
 
 -- Switch workspaces with mainMod + [1-5]
-for i = 1, 5 do
+for i = 1, 9 do
 	local key = tostring(i)
 	hl.bind(MainMod .. " + " .. key, function() return smw.workspace(i) end)
 	hl.bind(MainMod .. " + SHIFT + " .. key, function() return smw.move_to_workspace_silent(i) end)
